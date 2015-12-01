876b92e3a758caedac6f1440f970d330a4a66729
Jason Allen Gill <black6romeo@yahoo.com>
2015-11-30 21:13:47 -0600

    try it out 44x3

---
+++ try it out 44 x2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try_it_out_44_x3
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+            var checkWork = b * quotient + remainder;
+            Console.WriteLine(quotient);
+            Console.WriteLine(remainder);
+            Console.WriteLine(checkWork);
+        }
+    }
+}
